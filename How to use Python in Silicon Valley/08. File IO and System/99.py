dabe9386eb92fa93eedccde897f986ef29345edb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 14:40:33 +0000

    Adding tarfile member sanitization to extractall()

--- How to use Python in Silicon Valley/08. File IO and System/99.py
+++ How to use Python in Silicon Valley/08. File IO and System/99.py
@@ -24,6 +24,25 @@ with tarfile.open(file_path + 'test.tar.gz', 'w:gz') as tr:
 
 # tarfile 로 압축풀기
 with tarfile.open(file_path + 'test.tar.gz', 'r:gz') as tr:
-    tr.extractall(path='test_tar')
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tr, path="test_tar")
     with tr.extractfile('test_dir/sub_dir/sub_text.txt') as f:
         print(f.read())
